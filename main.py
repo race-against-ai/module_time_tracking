ebf8b877960b4354cfa71a8cff5c10b713cfcfcf
Justin Kurpeik <j.kurpeik@gmail.com>
2023-07-05 08:32:52 +0200

    changed the way the finish line is reset

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Copyright (C) 2022 NG:ITL
-from time_tracking.time_tracking import Timer
+from time_tracking.time_tracking import LapTimer
 
 if __name__ == '__main__':
     print("Time-tracking started")
-    timer = Timer()
+    timer = LapTimer()
     timer.start_timer()
     while True:
         timer.run()
