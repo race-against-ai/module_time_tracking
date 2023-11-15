772cf80ea5e72ca225b39cddb153442921df1319
Arne <111295854+ArneH666@users.noreply.github.com>
2023-11-15 10:13:15 +0100

    Added missing double-quote.

--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setup(
     url="https://github.com/vw-wob-it-edu-ngitl/raai_module_time_tracking",
     packages=find_packages(),
     long_description=read("README.md"),
-    install_requires=["pynng~=0.7.2", "opencv-python~=4.8.1.78],
+    install_requires=["pynng~=0.7.2", "opencv-python~=4.8.1.78"],
 )
